ab81155ab5006aecc883b34ecd46d94d1c0f19c0
robert-office <robertuillians123@gmail.com>
2022-07-12 15:18:23 -0300

    fix: resolvendo inconsistencias com dependecias, migrações do vite e etc...

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,7 @@
 <template>
 
-
+<h1>
+ oi eu estou na home
+</h1>
 
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { createApp, h } from 'vue';
 
 createInertiaApp({
-  resolve: name => require(`./Pages/${name}`),
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el)
-  },
-})
+    title: (title) => `${title} - ${appName}`,
+
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .mixin({ methods: { route } })
+            .mount(el);
+    },
+});
